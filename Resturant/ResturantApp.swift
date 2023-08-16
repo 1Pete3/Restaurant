2b8f754924e9f26cf0466e6b408ebaf766ce3b3d
Peter <1pete3learning@gmail.com>
2023-08-16 02:58:26 -0700

    Initial Commit

---
+++ Resturant/ResturantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResturantApp.swift
+//  Resturant
+//
+//  Created by Admin on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ResturantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
